7b16c48bb7824a0a13291badc7163ffe99e4fa6e
Manik Molla <mdblmanikislam777p@gmail.com>
2023-07-23 11:30:04 +0600

    created initial server with mongodb connection

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors');
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
+// middleware.....
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfpjg4p.mongodb.net/?retryWrites=true&w=majority`;
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello form Collage Website')
+})
+
+app.listen(port, () => {
+    console.log(`The website API is runing For Collage Website Service: ${port}`)
+})
